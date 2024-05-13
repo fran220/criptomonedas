19582ca203afb9a276fa96ded214dba94c842cdd
Fran <elfsandrada2@gmail.com>
2024-05-13 01:06:08 -0300

    se termino el proyecto

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
-const botonCotizar = document.querySelector('#cotizar');
+const formulario = document.querySelector('#form');
 const selectPais = document.querySelector('#pais');
 const selecCripto = document.querySelector('#moneda');
+const resultado = document.querySelector('#resultado');
+
+const objMonedas = {
+    cripto: '',
+    monedaPais: ''
+}
 
 document.addEventListener('DOMContentLoaded', ()=>{
-    llenarSelectPais();
+    llenarSelectMoneda();
     llenarSelectCripto();
-    botonCotizar.addEventListener('click', cotizar);
+    formulario.addEventListener('submit', validarCampos);
+    selecCripto.addEventListener('change', llenarObjMonedas);
+    selectPais.addEventListener('change', llenarObjMonedas)
+
 });
 
 
-function llenarSelectPais(){
-    const url = 'https://gist.githubusercontent.com/ssskip/5a94bfcd2835bf1dea52/raw/3b2e5355eb49336f0c6bc0060c05d927c2d1e004/ISO3166-1.alpha2.json';
+function llenarSelectMoneda(){
+    const url = 'https://raw.githubusercontent.com/vijinho/ISO-Country-Data/master/currencies.json';
 
     fetch(url)
         .then(res => res.json())
-        .then(result => optionPais(result))
+        .then(result => optionMoneda(result))
         .catch(err => console.log(err))
 }
 
@@ -27,18 +36,94 @@ function llenarSelectCripto(){
         .catch(err => console.log(err))
 }
 
-function optionPais(paises){
+function optionMoneda(paises){
+    paises.forEach(pais => {
+        const{name, code} = pais;
+        const option = document.createElement('option');
+        option.value = `${code}`;
+        option.innerHTML = `${name}`;
+        selectPais.appendChild(option);
+    });
 
 }
 
 function optionCripto(cripto){
-    
+    for(let i=0;i<cripto.Data.length;i++){
+        const {FullName, Name} = cripto.Data[i].CoinInfo;
+        const option = document.createElement('option');
+        option.value = `${Name}`;
+        option.innerHTML = `${FullName}`;
+        selecCripto.appendChild(option);
+    }
 }
 
-function cotizar(e){
-    e.preventDefault();
-    const monedaPais = document.querySelector('#pais').value;
-    const monedaVirtual = document.querySelector('#moneda').value;
-    console.log('cotizando..');
+function llenarObjMonedas(e){
+    objMonedas[e.target.name] = e.target.value;  
+}
 
+function validarCampos(e){
+    e.preventDefault();
+    const{cripto, monedaPais} = objMonedas;
+    if(cripto === '' || monedaPais === ''){
+        alert('Llene todos los campos')
+        return;
+    }
+
+    consultarApi();
+
+}
+
+function consultarApi(){
+    const{cripto, monedaPais} = objMonedas;
+    console.log(cripto)
+    const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${cripto}&tsyms=${monedaPais}`;
+
+    fetch(url)
+        .then(res => res.json())
+        .then(result => cotizar(result.DISPLAY[cripto][monedaPais]))
+        .catch(err => {
+            console.log(err)
+            alert('hubo un error en tu consulta pruebe con otra divisa o moneda virtual')
+        })
+}
+
+function cotizar(criptomoneda){
+    const { PRICE, HIGHDAY, LOWDAY, CHANGEPCT24HOUR, LASTUPDATE} = criptomoneda;
+
+    limipiarHtml();
+
+    console.log('price',PRICE);
+    console.log('valor mas alto del dia', HIGHDAY);
+    console.log('valor mas bajo del dia', LOWDAY);
+    console.log('variacion en las ultimas 42hs', CHANGEPCT24HOUR);
+    console.log('ultima actializacion', LASTUPDATE);
+
+    const price = document.createElement('p');
+    const valorAlto = document.createElement('p');
+    const valorBajo = document.createElement('p');
+    const variacion = document.createElement('p');
+    const ultimaActualizacion = document.createElement('p');
+
+    price.innerHTML = `Precio actual: <span>${PRICE}</span>`;
+    valorAlto.innerHTML = `Precio mas alto en el dia: <span>${HIGHDAY}</span>`;
+    valorBajo.innerHTML = `Precio mas bajo en el dia: <span>${LOWDAY}</span>`;
+    variacion.innerHTML = `Variacion: <span>${CHANGEPCT24HOUR}</span>`;
+    ultimaActualizacion.innerHTML = `Ultima actualizacion: <span>${LASTUPDATE}</span>`;
+
+    resultado.appendChild(price);
+    resultado.appendChild(valorAlto);
+    resultado.appendChild(valorBajo);
+    resultado.appendChild(variacion);
+    resultado.appendChild(ultimaActualizacion);
+}
+
+
+function spinner(){
+
+}
+
+function limipiarHtml(){
+    while(resultado.firstChild) {
+        resultado.removeChild(resultado.firstChild);
+    }
 }
